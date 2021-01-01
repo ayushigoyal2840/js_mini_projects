73a70f2c107958bb2e10c41b0d6ec2117ecccac2
Ayushi <ayushigoyal2840@gmail.com>
2021-01-01 18:20:49 +0530

    added new projects

--- javascipt mini projects/to-do list/main.js
+++ javascipt mini projects/to-do list/main.js
@@ -6,15 +6,19 @@ let addtaskbtn = document.getElementById('addtaskbtn');
 addtaskbtn.addEventListener("click", function() {
     addtaskinputval = addtaskinput.value;
     // console.log(addtaskinputval);
-    let webtask = localStorage.getItem("localtask");
-    if (webtask == null) {
-        taskobj = [];
+    if (addtaskinputval.trim() != 0) {
 
-    } else {
-        taskobj = JSON.parse(webtask);
+
+        let webtask = localStorage.getItem("localtask");
+        if (webtask == null) {
+            taskobj = [];
+
+        } else {
+            taskobj = JSON.parse(webtask);
+        }
+        taskobj.push(addtaskinputval);
+        localStorage.setItem("localtask", JSON.stringify(taskobj));
     }
-    taskobj.push(addtaskinputval);
-    localStorage.setItem("localtask", JSON.stringify(taskobj));
     showtask();
 
 })
@@ -34,7 +38,7 @@ function showtask() {
         html += ` <tr>
         <th scope="row">${index+1}</th>
         <td colspan="2" class="table-active">${item}</td>
-        <td><button style="border: none; background-color: white;" type="button" class="text-primary">Edit</button></td>
+        <td><button style="border: none;background-color: white;" type="button"  onclick="edittask(${index})"  class="text-primary">Edit</button></td>
         <td><button style="border: none; background-color: white;" type="button" class="text-danger">Delete</button></td>
     </tr>`;
 
@@ -44,3 +48,29 @@ function showtask() {
 
     addedtasklist.innerHTML = html;
 }
+
+function edittask(index) {
+    let saveindex = document.getElementById('saveindex');
+
+    let addtaskbtn = document.getElementById('addtaskbtn');
+    let savetaskbtn = document.getElementById('savetaskbtn');
+    saveindex.value = index;
+    let webtask = localStorage.getItem("localtask");
+    let taskobj = JSON.parse(webtask);
+    addtaskinput.value = taskobj[index];
+    addtaskbtn.style.display = "none";
+    savetaskbtn.style.display = "block";
+
+}
+let savetaskbtn = document.getElementById('savetaskbtn');
+savetaskbtn.addEventListener("click", function() {
+    let webtask = localStorage.getItem("localtask");
+    let taskobj = JSON.parse(webtask);
+    let saveindex = document.getElementById('saveindex').value;
+    taskobj[saveindex] = addtaskinput.value;
+    savetaskbtn.style.display = "none";
+    addtaskbtn.style.display = "block";
+    localStorage.setItem("localtask", JSON.stringify(taskobj));
+    addtaskinput.value = '';
+    showtask();
+})
